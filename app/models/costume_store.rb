f57515eca386cd70a943eef5ed2f495a545fbd2c
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-01 04:37:57 +0000

    automatically backed up by learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -2,7 +2,14 @@
 class CustomeStore
 
   def change
-    create_table customestores
+    create_table :customestores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
   end
 
 end
