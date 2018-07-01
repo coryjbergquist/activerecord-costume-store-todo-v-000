0cbf8b738f7024ba959e21a8c96e6db256a7a7e5
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-01 04:42:39 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -4,7 +4,11 @@
 class Costume
 
   def change
-    create_table costumes
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.string :image_url
   end
-  
+
 end
